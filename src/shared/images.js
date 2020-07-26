6876f7e6006c8d21e47ad85757b0c5243401266e
jaikumarkaushil <jaikumarkaushil@gmail.com>
2020-07-26 23:40:57 +0530

    second commit

--- src/shared/images.js
+++ src/shared/images.js
@@ -1,19 +1,19 @@
 const Images = [
     {
         id: 0,
-        image: "https://picsum.photos/900/800?random=1"
+        image: "https://picsum.photos/900/600?random=1"
     },
     {
         id: 1,
-        image: "https://picsum.photos/900/800?random=2"
+        image: "https://picsum.photos/900/600?random=2"
     },
     {
         id: 2,
-        image: "https://picsum.photos/900/800?random=3"
+        image: "https://picsum.photos/900/600?random=3"
     },
     {
         id: 3,
-        image: "https://picsum.photos/900/800?random=4"
+        image: "https://picsum.photos/900/600?random=4"
     }
 ]
 
